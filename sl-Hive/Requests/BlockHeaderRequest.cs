945256a4dfc52a064c5b963f91e2b484d2d09b9f
farpetrad <farpetrad@splinterlands.com>
2022-10-14 10:33:12 -0700

    block header

---
+++ sl-Hive/Requests/BlockHeaderRequest.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using sl_Hive.Attributes;
+
+namespace sl_Hive.Requests
+{
+    //get_block_header
+    [RPCMethod("condenser_api", "get_block_header")]
+    public class BlockHeaderRequest : HiveJsonRequest
+    {
+        [JsonProperty("params")]
+        public List<Int64> BlockNumber { get; set; } = new List<Int64>();
+    }
+}
