e3de83fd989b7813055acc45fd180677080c784e
farpetrad <farpetrad@splinterlands.com>
2022-10-13 18:19:10 -0700

    Add more types

--- sl-Hive/Models/Block.cs
+++ sl-Hive/Models/Block.cs
@@ -8,9 +8,9 @@ namespace sl_Hive.Models
         public DateTime? Timestamp { get; set; } = null;
         public string Witness { get; set; } = string.Empty;
         public string Transaction_Merkle_Root { get; set; } = string.Empty;
-        public List<object> Extensions { get; set; } = new List<object>();
+        public List<JObject> Extensions { get; set; } = new List<JObject>();
         public string Witness_Signature { get; set; } = string.Empty;
-        public List<JObject> Transactions { get; set; } = new List<JObject>();
+        public List<HiveTransaction> Transactions { get; set; } = new List<HiveTransaction>();
         public string Block_Id { get; set; } = string.Empty;
         public string Signing_Key { get; set; } = string.Empty;
         public List<string> Transaction_Ids { get; set; } = new List<string>();
---
+++ sl-Hive/Models/HiveTransaction.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sl_Hive.Models
+{
+    public class HiveTransaction
+    {
+        public string Ref_Block_Num { get; set; } = string.Empty;
+        public string Ref_Block_Prefix { get; set; } = string.Empty;
+        public DateTime? Expiration { get; set; } = null;
+        public List<JArray> Operations { get; set; } = new List<JArray>();
+        public List<JObject> Extensions { get; set; } = new List<JObject>();
+        public List<string> Signatures { get; set; } = new List<string>();
+        public string Transaction_id { get; set; } = string.Empty;
+        public Int64 Block_Num { get; set; } = -1;
+        public Int64 Transaction_Num { get; set; } = -1;
+    }
+}
