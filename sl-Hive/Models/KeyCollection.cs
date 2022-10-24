b9e9fb382fd94d9137c905c54b6d7604aa1ad473
Jonas Frost <j@duplo.org>
2022-10-25 01:06:51 +0700

    empty array

--- sl-Hive/Models/KeyCollection.cs
+++ sl-Hive/Models/KeyCollection.cs
@@ -5,7 +5,7 @@ namespace sl_Hive.Models
     public class KeyCollection
     {
         public int Weight_Threshold { get; set; } = 0;
-        public IReadOnlyList<JsonArray> Account_Auths { get; set; } = new List<JsonArray>();
-        public IReadOnlyList<JsonArray> Key_Auths { get; set; } = new List<JsonArray>();
+        public IReadOnlyList<JsonArray> Account_Auths { get; set; } = Array.Empty<JsonArray>();
+        public IReadOnlyList<JsonArray> Key_Auths { get; set; } = Array.Empty<JsonArray>();
     }
 }
