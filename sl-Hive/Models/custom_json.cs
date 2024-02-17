9704692a93d58f9b09b9acedea1e6f98c4a69b2d
farpetrad-sl <farpetrad@splinterlands.com>
2024-02-17 08:45:44 -0700

    rename

---
+++ sl-Hive/Models/custom_json.cs
@@ -0,0 +1,17 @@
+﻿using sl_Hive.Requests;
+using System.Text.Json.Serialization;
+
+namespace sl_Hive.Models;
+
+public class custom_json: IOperationID
+{
+    public int opid => 18;
+
+    public required string[] required_auths;
+
+    public required string[] required_posting_auths;
+
+    public required string id;
+
+    public required string json;
+}
