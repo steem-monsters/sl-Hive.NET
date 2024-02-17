0bafd959f2aa8499737a7756a547308aead4c4f6
farpetrad-sl <farpetrad@splinterlands.com>
2024-02-17 08:45:53 -0700

    fix warnings

--- sl-Hive/Models/BlockHeader.cs
+++ sl-Hive/Models/BlockHeader.cs
@@ -4,10 +4,10 @@ namespace sl_Hive.Models
 {
     public class BlockHeader
     {
-        public string Previous { get; set; }
+        public string Previous { get; set; } = string.Empty;
         public DateTime? TimeStamp { get; set; }
-        public string Witness { get; set; }
-        public string Transaction_Merkle_Root { get; set; }
-        public IReadOnlyList<JsonNode> Extensions { get; set; }
+        public string Witness { get; set; } = string.Empty;
+        public string Transaction_Merkle_Root { get; set; } = string.Empty;
+        public IReadOnlyList<JsonNode> Extensions { get; set; } = new List<JsonNode>();
     }
 }
